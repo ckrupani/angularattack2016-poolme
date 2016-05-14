abb68cef1065319e67e1158eb084ab2323faec86
unknown <ZW78@INLTD11006.global.tesco.org>
2016-05-14 15:25:05 +0530

    Implemented empty templates for different views.

---
+++ app/components/dashboard/myTrips/myTrips.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'pm-mytrips',
+    templateUrl: 'app/components/dashboard/myTrips/myTrips.component.html',
+    styleUrls: ['app/components/dashboard/myTrips/myTrips.component.css']
+})
+
+export class PmMyTripsComponent {}
---
+++ app/components/dashboard/profile/profile.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'pm-profile',
+    templateUrl: 'app/components/dashboard/profile/profile.component.html',
+    styleUrls: ['app/components/dashboard/profile/profile.component.css']
+})
+
+export class PmProfileComponent {}
