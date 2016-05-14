e4d388d5e97cf146f0d03e3153234ee0621d9184
unknown <ZW78@INLTD11006.global.tesco.org>
2016-05-14 06:31:11 +0530

    Set up JSPM for the project.

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'pool-me-app',
+    template: `<h1>AngularAttack 2016</h1><p>Your project runs!</p>`
+})
+
+export class AppComponent {}
---
+++ app/bootstrap.ts
@@ -0,0 +1,6 @@
+import 'zone.js';
+import 'reflect-metadata';
+import 'es6-shim';
+import 'bootstrap';
+import 'bootstrap/css/bootstrap.css!';
+import 'app/main';
