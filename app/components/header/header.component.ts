b5ac627920c356fed5e9852215395d89d1f2af45
unknown <ZW78@INLTD11006.global.tesco.org>
2016-05-14 07:22:41 +0530

    Implemented header, content and footer areas.

---
+++ app/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+	selector: 'pm-header',
+	templateUrl: 'app/components/header/header.component.html',
+	styleUrls: ['app/components/header/header.component.css']
+})
+
+export class PmHeaderComponent {}
